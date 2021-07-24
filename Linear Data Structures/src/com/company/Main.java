ae72c07093e1ae80000d850972540942337d53cf
iNetzov <ivn9812@gmail.com>
2021-07-24 15:23:26 +0300

    creating own dimanic list Array

---
+++ Linear Data Structures/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    class DynamicArray<T>{
+        Object[] values = new Object[43];
+
+    }
+
+    public static void main(String[] args) {
+	// write my own array list
+    }
+}
